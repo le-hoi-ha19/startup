dc5be9f9635e3ce12576a6d1bf5574c8f3b590e8
le-hoi-ha19 <hale85528@.com>
2023-10-03 07:56:22 +0700

    'complete-th02'

--- startup/Models/Menu.cs
+++ startup/Models/Menu.cs
@@ -8,13 +8,13 @@ namespace startup.Models
     {
         [Key]
         public int MenuID { get; set; }
-        public string MenuName { get; set; }
+        public string? MenuName { get; set; }
         public bool? IsActive { get; set; }
         public string? ControllerName { get; set; }
         public string? ActionName { get; set; }
         public int Levels { get; set; }
         public int ParentID { get; set; }   
-        public string Link { get; set; }    
+        public string? Link { get; set; }    
         public int MenuOrder { get; set; }
         public int Position { get; set; }
 
